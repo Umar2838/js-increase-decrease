5afc627d1dd29cef14bdfdc8cd68d50aabc625f3
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-01-26 21:22:14 +0500

    added increase and decrease

---
+++ app.js
@@ -0,0 +1,13 @@
+// Chapter 01 (alert)
+
+// Question 01
+alert("Hello visitors Wellcome to my website.")
+// Question 02
+alert("Error! please enter a valid password.")
+// Question 03
+alert("Welcome to JS land...\nHappy Coding!")
+// Question 04
+alert("Wecome to JS Land...")
+alert("Happy Coding\nprevent this page from creating additional dailogs.")
+//Question 05
+alert("Hello...I can run through my web browser's console")
